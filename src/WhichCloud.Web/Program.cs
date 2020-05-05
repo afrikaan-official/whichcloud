da7eca5d05f3ed526c547d8a1bf7437856412a5d
kaan <kaanyurdakok@yandex.com>
2020-05-05 20:56:38 +0300

    Configure port

--- src/WhichCloud.Web/Program.cs
+++ src/WhichCloud.Web/Program.cs
@@ -18,6 +18,11 @@ namespace WhichCloud.Web
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://*:80");
+                });
     }
 }
